0a5609986006cb903c75ee623cdd0fd3a9deab1d
Shashaank N <panda-truth-scuba@duck.com>
2025-09-20 23:47:57 -0400

    Add MNAR model extensions

--- src/promfam/models.py
+++ src/promfam/models.py
@@ -34,9 +34,6 @@ def _get_pheno_categories(Y: torch.Tensor) -> dict[str, torch.Tensor]:
 # Base implementation
 # used for all models
 class Base:
-    def __init__(self):
-        pass
-
     # Model structure
     def model():
         raise NotImplementedError
@@ -91,7 +88,18 @@ class Base:
                 obs = pyro.sample(f"obs_{cat}", obs_dist, obs = torch.nan_to_num(Y[:, pheno_mask]))
     
     # Fit model to given data
-    def fit(self, Y: torch.Tensor, K: int):
+    def fit(
+        self, 
+        Y: torch.Tensor, 
+        K: int, 
+        mnar_by_data: bool = False
+    ):
+        # Add missingness to data if enabled
+        Y = Y.float()
+        if mnar_by_data:
+            mask = (~torch.isnan(Y)).float()
+            Y = torch.cat(torch.cat([Y, mask], axis = 1))
+
         # Get phenotype categories
         pheno_cat = _get_pheno_categories(Y)
 
@@ -110,11 +118,52 @@ class Base:
 
         return samples, loss
 
+# Base implementation used for MNAR models
+class BaseMNAR(Base):
+    # Likelihood function used
+    # in MNAR models
+    def likelihood(
+        self,
+        W: torch.Tensor,
+        sigma_W: torch.Tensor, 
+        Y: torch.Tensor,
+        M: torch.Tensor,
+        pheno_cat: dict[str, torch.Tensor]
+    ):
+        super().likelihood(W, Y, pheno_cat)
+        N, P = M.shape
+        with pyro.plate("P", P):
+            with pyro.plate("N", N):
+                obs_m = pyro.sample('obs_m', dist.Bernoulli(sigma_W), obs = M)
+
+    # Fit model to given data
+    def fit(
+        self, 
+        Y: torch.Tensor,
+        M: torch.Tensor, 
+        K: int, 
+    ):
+        # Get phenotype categories
+        pheno_cat = _get_pheno_categories(Y)
+
+        # Initialize setup for SVI
+        guide = AutoDiagonalNormal(self.model)
+        params = {
+            "Y": Y,
+            "M": M,
+            "K": K,
+            "pheno_cat": pheno_cat
+        }
+
+        # Run SVI and return average of samples 
+        # from predicted posterior
+        loss = svi(self.model, guide, params, iterations = 1000, print_every = 100)
+        samples = svi_posterior(self.model, guide, params, num_samples = 50)
+
+        return samples, loss
+
 # Normal FA model
 class Normal(Base):
-    def __init__(self):
-        super().__init__()
-
     # Model structure
     def model(
         self,
@@ -139,9 +188,6 @@ class Normal(Base):
 
 # Dirichlet-Beta FA model
 class DirichletBeta(Base):
-    def __init__(self):
-        super().__init__()
-
     # Model structure
     def model(
         self,
@@ -166,9 +212,6 @@ class DirichletBeta(Base):
 
 # Dirichlet-Normal FA model
 class DirichletNormal(Base):
-    def __init__(self):
-        super().__init__()
-
     # Model structure
     def model(
         self,
@@ -193,9 +236,6 @@ class DirichletNormal(Base):
 
 # Lognormal FA model
 class Lognormal(Base):
-    def __init__(self):
-        super().__init__()
-
     # Model structure
     def model(
         self,
@@ -217,3 +257,49 @@ class Lognormal(Base):
         W = 1 - torch.exp(-(U @ V.T))
 
         self.likelihood(W, Y, pheno_cat)
+
+# Normal MNAR FA model
+class NormalMNAR(BaseMNAR):
+    # Model structure
+    def model(
+        self,
+        Y: torch.Tensor,
+        M: torch.Tensor,
+        K: int,
+        pheno_cat: dict[str, torch.Tensor]
+    ):
+        N, P = Y.shape
+
+        # μ_U ~ N(0, 1)
+        mu_U_dist = dist.Normal(torch.tensor(0., device = Y.device), 1.).expand([N, K]).to_event(2)
+        mu_U = pyro.sample("mu_U", mu_U_dist)
+
+        # μ_V ~ N(0, 1)
+        mu_V_dist = dist.Normal(torch.tensor(0., device = Y.device), 1.).expand([P, K]).to_event(2)
+        mu_V = pyro.sample("mu_V", mu_V_dist)
+
+        # Σ_U ~ N(0, 1)
+        sigma_U_dist = dist.Normal(torch.tensor(0., device = Y.device), 1.).expand([N, K]).to_event(2)
+        sigma_U = pyro.sample("sigma_U", sigma_U_dist)
+
+        # Σ_V ~ N(0, 1)
+        sigma_V_dist = dist.Normal(torch.tensor(0., device = Y.device), 1.).expand([P, K]).to_event(2)
+        sigma_V = pyro.sample("sigma_V", sigma_V_dist)
+
+        # U_unit ~ N(0, 1)
+        # U = μ_U + U_unit * exp(-Σ_U)
+        U_unit_dist = dist.Normal(torch.tensor(0., device = Y.device), 1.).expand([N, K]).to_event(2)
+        U_unit = pyro.sample("U_unit", U_unit_dist)
+        U = pyro.deterministic("U", mu_U + U_unit * torch.exp(-sigma_U))
+
+        # V_unit ~ N(0, 1)
+        # V = μ_V + V_unit * exp(-Σ_V)
+        V_unit_dist = dist.Normal(torch.tensor(0., device = Y.device), 1.).expand([P, K]).to_event(2)
+        V_unit = pyro.sample("V_unit", V_unit_dist)
+        V = pyro.deterministic("V", mu_V + V_unit * torch.exp(-sigma_V))
+
+        # f(U, V) = σ(UV^T)
+        W = torch.sigmoid(U @ V.T)
+        sigma_W = torch.sigmoid(sigma_U @ sigma_V.T)
+
+        self.likelihood(W, sigma_W, Y, M, pheno_cat)
