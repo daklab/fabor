c40d6d396a437509c6c43d9cca6ec7605f33fb31
Shashaank N <panda-truth-scuba@duck.com>
2025-10-10 11:26:45 -0400

    Modify ordinal likelihood and SVI guides
    Use OrderedLogistic for ordinal likelihood, use AutoDelta guide for Dirichlet latent variables and AutoDiagonalNormal for all others

--- fabor/base.py
+++ fabor/base.py
@@ -1,7 +1,10 @@
 import torch
 import pyro
 import pyro.distributions as dist
-from pyro.infer.autoguide import AutoDiagonalNormal
+import pyro.distributions.transforms as transforms
+from pyro.infer.reparam import TransformReparam
+from pyro import poutine
+from pyro.infer.autoguide import AutoGuideList, AutoDiagonalNormal, AutoDelta
 
 from .fit import svi, svi_posterior
 
@@ -30,6 +33,22 @@ def get_pheno_categories(Y: torch.Tensor) -> dict[str, torch.Tensor]:
             pheno_cat[f'ordinal_{count}'] = pheno_counts
 
     return pheno_cat
+
+# Mark parameters sampled from
+# Dirichlet prior to be handled
+# differently in SVI
+def dirichlet_selector(msg):
+    if msg['type'] == 'sample':
+        if msg['fn'].has_rsample:
+            return isinstance(msg['fn'].base_dist, dist.Dirichlet)
+    return False
+
+# Logit function with clamping
+# to prevent overflow
+def safe_logit(x: torch.Tensor):
+    eps = torch.finfo(x.dtype).eps
+    y = torch.clamp(x, eps, 1 - eps)
+    return torch.logit(y)
 
 # Base implementation
 # used for all models
@@ -62,30 +81,26 @@ class Base:
                 obs_dist = dist.Bernoulli(W[:, pheno_mask]).mask(mask[:, pheno_mask])
             else: # Ordinal phenotypes
                 # s_p ~ Dirichlet(1 / D_p)
+                # c_p = Σ^{d}_{a=1} σ^-1(s_{pa})
                 D_p = int(cat.split('_')[-1])
                 alpha = torch.ones((D_p,), device = mask.device)
-                s_dist = dist.Dirichlet(alpha).expand([Q]).to_event(1)
-                s = pyro.sample(f"s_{cat}", s_dist)
-
-                # c_p = Σ^{d}_{a=1} s_{pa}
-                c = torch.cumsum(s[..., :-1], dim = -1)
-
-                # q_{np} = 1 / 2(c_p - W_{np}) + 1 / 2
-                q = 0.5 * (c - W[:, pheno_mask].unsqueeze(-1)) + 0.5
-
+                with poutine.reparam(config = {f"c_{cat}": TransformReparam()}):
+                    c_obs = pyro.sample(
+                        f"c_{cat}",
+                        dist.TransformedDistribution(
+                            dist.Dirichlet(alpha).expand([Q]).to_event(1),
+                            transforms.SimplexToOrderedTransform(torch.tensor(0., device = mask.device))
+                        ),
+                    )
+                # q_{np} = σ(c_p - σ^-1(W_{np}))
                 # θ_{npd} = q_{npd} - q_{np(d - 1)}
-                theta = torch.zeros((N,) + s.shape, device = mask.device)
-                theta[..., 0] = q[..., 0]
-                theta[..., 1:-1] = q[..., 1:] - q[..., :-1]
-                theta[..., -1] = 1 - q[..., -1]
-
                 # Y_{np} ~ Cat(θ_{np})
-                obs_dist = dist.Categorical(theta).mask(mask[:, pheno_mask])
+                obs_dist = dist.OrderedLogistic(safe_logit(W[:, pheno_mask]), c_obs).mask(mask[:, pheno_mask])
 
-        # Likelihood of observed variable
-        with pyro.plate(f"Q_{cat}", Q):
-            with pyro.plate(f"N_{cat}", N):
-                obs = pyro.sample(f"obs_{cat}", obs_dist, obs = torch.nan_to_num(Y[:, pheno_mask]))
+            # Likelihood of observed variable
+            with pyro.plate(f"Q_{cat}", Q):
+                with pyro.plate(f"N_{cat}", N):
+                    obs = pyro.sample(f"obs_{cat}", obs_dist, obs = torch.nan_to_num(Y[:, pheno_mask]))
 
     # Fit model to given data
     def fit(
@@ -101,13 +116,19 @@ class Base:
         Y = Y.float()
         if mnar_by_data:
             mask = (~torch.isnan(Y)).float()
-            Y = torch.cat(torch.cat([Y, mask], axis = 1))
+            Y = torch.cat([Y, mask], axis = 1)
 
         # Get phenotype categories
         pheno_cat = get_pheno_categories(Y)
 
-        # Initialize setup for SVI
-        guide = AutoDiagonalNormal(self.model)
+        # Initialize guide for SVI
+        guide = AutoGuideList(self.model)
+        # For non-Dirichlet variables
+        guide.append(AutoDiagonalNormal(poutine.block(self.model, hide_fn = dirichlet_selector)))
+        # For Dirichlet variables
+        guide.append(AutoDelta(poutine.block(self.model, expose_fn = dirichlet_selector)))
+
+        # Compile input parameters
         params = {
             "Y": Y,
             "K": K,
@@ -158,8 +179,14 @@ class BaseMNAR(Base):
         # Get phenotype categories
         pheno_cat = get_pheno_categories(Y)
 
-        # Initialize setup for SVI
-        guide = AutoDiagonalNormal(self.model)
+        # Initialize guide for SVI
+        guide = AutoGuideList(self.model)
+        # For non-Dirichlet variables
+        guide.append(AutoDiagonalNormal(poutine.block(self.model, hide_fn = dirichlet_selector)))
+         # For Dirichlet variables
+        guide.append(AutoDelta(poutine.block(self.model, expose_fn = dirichlet_selector)))
+
+        # Compile input parameters
         params = {
             "Y": Y,
             "M": M,
